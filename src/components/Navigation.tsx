5b08104cb4d3ea444fce6cc5bb084ccf728589f3
chinchillagarry <chinchillagarry2003@gmail.com>
2025-03-13 14:08:02 -0700

    hhhh

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,21 +24,21 @@ export default function Navigation() {
             </Link>
             <div className="hidden md:flex items-center space-x-4">
               <Link
-                to="/speakers"
+                to="/speakerportal"
                 className="flex items-center space-x-2 text-gray-600 hover:text-blue-500"
               >
                 <Mic2 size={20} />
                 <span>Speakers</span>
               </Link>
               <Link
-                to="/volunteers"
+                to="/volunteermanagement"
                 className="flex items-center space-x-2 text-gray-600 hover:text-blue-500"
               >
                 <Users size={20} />
                 <span>Volunteers</span>
               </Link>
               <Link
-                to="/vendors"
+                to="/vendormanagement"
                 className="flex items-center space-x-2 text-gray-600 hover:text-blue-500"
               >
                 <Store size={20} />
